2fdada8ab211f961b5559b4358c01d0f38de8765
jessl314 <jlchrysanthemum456@gmail.com>
2025-01-10 17:54:59 -0800

    imported bootstrapdist thing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Button } from "react-bootstrap"
 
 // deleted App.css
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <Button className="m-auto text-center" variant="primary">Hello</Button>
+
       
     </>
   )
